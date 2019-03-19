bf1c3f61fc5be580412a8efea3f1e870a9996cc1
Javier Muñoz <38103216+JavierMunozb@users.noreply.github.com>
2019-03-19 19:33:56 +0100

    Update Weapon.java

--- Videogame/src/Entities/Items/Weapon.java
+++ Videogame/src/Entities/Items/Weapon.java
@@ -9,17 +9,9 @@ package Entities.Items;
  *
  * @author Javier Muñoz
  */
-<<<<<<< HEAD:Videogame/src/Characters/Weapon.java
 public abstract class Weapon extends Item {
-    private int damage;
-    public abstract void use();
-    public abstract boolean available();
-=======
-public abstract class Weapon extends Item
-{
     protected int damage;
     
->>>>>>> 273b1846556215a9f0075e0f0751b3d0e5b18867:Videogame/src/Entities/Items/Weapon.java
     public Weapon(String id, int damage)
     {
         super(id);
