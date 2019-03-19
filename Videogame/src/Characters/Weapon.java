e586f143d41c3c182d4893f3b7735435e8d4ae08
Javier Muñoz <38103216+JavierMunozb@users.noreply.github.com>
2019-03-19 12:00:20 +0100

    Created the Weapon class
    A type of Item that the Playable Character might have in their inventory

---
+++ Videogame/src/Characters/Weapon.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Characters;
+
+/**
+ *
+ * @author Javier Muñoz
+ */
+public class Weapon extends Item
+{
+    private int damage;
+    
+    public Weapon(String id, int damage)
+    {
+        super(id);
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+    
+    
+}
