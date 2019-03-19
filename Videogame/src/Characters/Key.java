be954abb9a42e70d2e9c223bdae161f6803ec6e3
Javier Muñoz <38103216+JavierMunozb@users.noreply.github.com>
2019-03-19 19:09:19 +0100

    Created the Key class

---
+++ Videogame/src/Characters/Key.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Characters;
+
+/**
+ *
+ * @author Javier Muñoz
+ */
+public class Key extends Item
+{
+    private String name;
+    
+    public Key(String name, String id)
+    {
+        super(id);
+        this.name = name;
+    }
+    
+    public String toString()
+    {
+        return "This is a key. It has a name tag: "+name;
+    }
+}
