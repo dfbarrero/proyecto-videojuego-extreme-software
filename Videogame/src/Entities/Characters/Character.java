273b1846556215a9f0075e0f0751b3d0e5b18867
Javier Muñoz <38103216+JavierMunozb@users.noreply.github.com>
2019-03-19 19:16:41 +0100

    Moved the classes to a separate folder for better organization

--- Videogame/src/Entities/Characters/Character.java
+++ Videogame/src/Entities/Characters/Character.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Entities;
+package Entities.Characters;
 /**
  *
  * @author Javier Muñoz
