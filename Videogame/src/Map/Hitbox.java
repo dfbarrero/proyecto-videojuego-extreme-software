2f60cbc0cfd3d7fab5e58f30e66379039eb7f1a1
juangomezh <38103170+juangomezh@users.noreply.github.com>
2019-04-05 19:49:15 +0200

    Add files via upload

--- Videogame/src/Map/Hitbox.java
+++ Videogame/src/Map/Hitbox.java
@@ -9,8 +9,9 @@ import org.newdawn.slick.geom.Rectangle;
 public class Hitbox {
 
     private Rectangle hitbox;
-
+    private float posx, posy;
     public Hitbox(float x, float y, float width, float height) {
+        posx=x; posy=y;
         hitbox = new Rectangle(x, y, width, height);
     }
 
@@ -23,7 +24,24 @@ public class Hitbox {
     }
 
     public void updatePos(float x, float y) {
-        hitbox.setY(hitbox.getY() + y);
-        hitbox.setX(hitbox.getX() + x);
+        hitbox.setY(posx + y);
+        hitbox.setX(posy + x);
     }
+
+    public float getPosx() {
+        return posx;
+    }
+
+    public void setPosx(float posx) {
+        this.posx = posx;
+    }
+
+    public float getPosy() {
+        return posy;
+    }
+
+    public void setPosy(float posy) {
+        this.posy = posy;
+    }
+    
 }
