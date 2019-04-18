6a7cfde8d7f2bf61ce920cb1ab912830ffbc15b1
juangomezh <38103170+juangomezh@users.noreply.github.com>
2019-04-18 22:14:54 +0200

    Add files via upload

--- Videogame/src/Map/Hitbox.java
+++ Videogame/src/Map/Hitbox.java
@@ -3,15 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Map;
+package Play;
 import org.newdawn.slick.geom.Rectangle;
 
 public class Hitbox {
 
     private Rectangle hitbox;
-    private float posx, posy;
+
     public Hitbox(float x, float y, float width, float height) {
-        posx=x; posy=y;
         hitbox = new Rectangle(x, y, width, height);
     }
 
@@ -24,24 +23,7 @@ public class Hitbox {
     }
 
     public void updatePos(float x, float y) {
-        hitbox.setY(posx + y);
-        hitbox.setX(posy + x);
+        hitbox.setY(hitbox.getY() + y);
+        hitbox.setX(hitbox.getX() + x);
     }
-
-    public float getPosx() {
-        return posx;
-    }
-
-    public void setPosx(float posx) {
-        this.posx = posx;
-    }
-
-    public float getPosy() {
-        return posy;
-    }
-
-    public void setPosy(float posy) {
-        this.posy = posy;
-    }
-    
 }
--- Videogame/src/Map/Mapa.java
+++ Videogame/src/Map/Mapa.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Map;
+package Play;
 
 /**
  *
@@ -16,20 +16,22 @@ import java.util.logging.Logger;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.tiled.TiledMap;
 
 public class Mapa {
-
+    private double x, y;
     private TiledMap map;
     private ArrayList<Hitbox> blocks;   
     private Hitbox Character;
-    public Mapa(String ruta) {
+    public Mapa(String ruta, GameContainer gc) {
         try {
             this.map = new TiledMap(ruta);
             //Carga de elementos del mapa
             blocks = new ArrayList<>();
-            Character=new Hitbox(32, 32, 32, 32);
+            Character=new Hitbox(gc.getWidth()/2, gc.getHeight()/2, 30, 30);
             cargaMuros();        /*TODO: cargaSaltosEstado */
         } catch (SlickException ex) {
             Logger.getLogger(Mapa.class.getName()).log(Level.SEVERE, null, ex);
@@ -49,7 +51,6 @@ public class Mapa {
     }
     private void cargaMuros() {
         int collisions = map.getLayerIndex("Walls"); //TODO: definir otro nombre para la capa
-
         if (collisions != -1) {    //Si encuentra la capa
             for (int j = 0; j < map.getHeight(); j++) {
                 for (int i = 0; i < map.getWidth(); i++) {
@@ -61,24 +62,78 @@ public class Mapa {
         }
     }
     public void actualizarPersonaje(float x, float y) {
-        boolean collision;
         Character.updatePos(x, y);
     }
-    public void actualizarBloques(float x, float y)
+    public void Movimiento(int i, GameContainer gc)
+    {
+        if (gc.getInput().isKeyDown(Input.KEY_W) || gc.getInput().isKeyDown(Input.KEY_UP)) {
+            y += i*.1f;  //i=tiempo de update
+            actualizarMuros(0, +(i*.1f));
+            collisions(i, gc, "ARRIBA");
+        }
+        if (gc.getInput().isKeyDown(Input.KEY_S) || gc.getInput().isKeyDown(Input.KEY_DOWN)) {
+            y -= i*.1f;  //i=tiempo de update
+            actualizarMuros(0, -(i*.1f));
+            collisions(i, gc, "ABAJO");
+        }
+        if (gc.getInput().isKeyDown(Input.KEY_A) || gc.getInput().isKeyDown(Input.KEY_LEFT)) {
+            x += i*.1f;  //i=tiempo de update
+            actualizarMuros(+(i*.1f), 0);
+            collisions(i, gc, "IZQ");
+        }
+        if (gc.getInput().isKeyDown(Input.KEY_D) || gc.getInput().isKeyDown(Input.KEY_RIGHT)) {
+            x -= i*.1f;  //i=tiempo de update
+            actualizarMuros(-(i*.1f), 0);
+            collisions(i, gc, "DCHA");
+        }
+    }
+    public void actualizarMuros(float x, float y)
     {
         for(int i=0;i<blocks.size();i++)
         {
             blocks.get(i).updatePos(x, y);
         }
     }
-    public void renderMap(GameContainer gc, double x, double y, Graphics grphcs, boolean ver_hitbox) {
-        map.render((int) x, (int) y, 0, 0, gc.getWidth(), gc.getHeight());
+    public void renderMap(GameContainer gc, Graphics grphcs, boolean ver_hitbox) {
+        map.render((int) this.x, (int) this.y, 0, 0, gc.getWidth(), gc.getHeight());
+        boolean amarillo=true;
         for (int i=0;i<blocks.size();i++) {
                 grphcs.setColor(Color.black);
                 grphcs.drawRect(blocks.get(i).getRectangulo().getX(), blocks.get(i).getRectangulo().getY(), blocks.get(i).getRectangulo().getWidth(), blocks.get(i).getRectangulo().getHeight());
             }
             grphcs.drawRect(Character.getRectangulo().getX(), Character.getRectangulo().getY(), Character.getRectangulo().getHeight(), Character.getRectangulo().getWidth());
         }
+    public void collisions(int i, GameContainer gc, String dir)
+    {
+        for(int j=0;j<blocks.size();j++)
+        {
+            if(blocks.get(j).getRectangulo().intersects(Character.getRectangulo()))
+            {
+                if(dir.toUpperCase().equals("ABAJO"))
+                {
+                    y += i*.1f;  //i=tiempo de update
+                    actualizarMuros(0, (i*.1f));
+                }
+                else if(dir.toUpperCase().equals("ARRIBA"))
+                {
+                    y -= i*.1f;  //i=tiempo de update
+                    actualizarMuros(0, -(i*.1f));
+                }
+                else if(dir.toUpperCase().equals("DCHA"))
+                {
+                    x += i*.1f;  //i=tiempo de update
+                    actualizarMuros((i*.1f), 0);
+                     
+                }
+                else if(dir.toUpperCase().equals("IZQ"))
+                {
+                    x -= i*.1f;  //i=tiempo de update
+                    actualizarMuros(-(i*.1f), 0);
+                }
+            }
+        }
     }
+
+}
    
 
