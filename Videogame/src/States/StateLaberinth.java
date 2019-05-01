723c62c40f69ee6fd756f05176b08d8d14f8dc75
Javier Muñoz <38103216+JavierMunozb@users.noreply.github.com>
2019-05-01 19:29:04 +0200

    Corrected typo

--- Videogame/src/States/StateLaberinth.java
+++ Videogame/src/States/StateLaberinth.java
@@ -50,7 +50,7 @@ public class StateLaberinth extends BasicGameState{
     public int getID() {
         return 21;
     }
-public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
         Char=new PlayableCharacter("id",(float) gc.getWidth()/2,(float) gc.getHeight()/2, "pCName", 15, 100);
         map=new Mapa("src/Tiled/Laberinth.tmx", gc, Char, npcs, enemy);
         int positionx=-625, positiony=-405;
