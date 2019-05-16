8f7db6e0d3d699d1504e65553986f482c043c8af
juangomezh <38103170+juangomezh@users.noreply.github.com>
2019-05-16 11:29:44 +0200

    Add files via upload

---
+++ Videogame/src/Tiled/Cofres.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Cofres" tilewidth="32" tileheight="32" tilecount="578" columns="34">
+ <image source="Cofres_lab.png" trans="0c000c" width="1099" height="550"/>
+</tileset>
---
+++ Videogame/src/Tiled/Fragmentos.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Fragmentos" tilewidth="32" tileheight="32" tilecount="900" columns="30">
+ <image source="fragmentos.gif" trans="0c000c" width="960" height="960"/>
+</tileset>
