8ca08fbc870941868e891718ed21b4dd6d6c2408
juangomezh <38103170+juangomezh@users.noreply.github.com>
2019-05-20 22:59:02 +0200

    Update End.java

--- Videogame/src/States/End.java
+++ Videogame/src/States/End.java
@@ -49,10 +49,6 @@ import org.lwjgl.input.Mouse;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
-/**
- *
- * @author razvanvc
- */
 public class End extends BasicGameState{
 private TextDisplay td = null;
     private String texto;
