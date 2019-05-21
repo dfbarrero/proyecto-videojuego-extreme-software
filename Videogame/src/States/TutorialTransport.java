ab54b56ef8c23aa9d308cb97fde01a1d743a4687
juangomezh <38103170+juangomezh@users.noreply.github.com>
2019-05-21 12:00:22 +0200

    Update TutorialTransport.java

--- Videogame/src/States/TutorialTransport.java
+++ Videogame/src/States/TutorialTransport.java
@@ -49,10 +49,6 @@ import org.lwjgl.input.Mouse;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
-/**
- *
- * @author razvanvc
- */
 public class TutorialTransport extends BasicGameState{
 
     public String mouse = "No input yet!";
